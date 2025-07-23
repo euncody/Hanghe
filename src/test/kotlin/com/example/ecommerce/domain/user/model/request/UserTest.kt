971768c2b8f03e6eec8b4fabc4c578efd1aa8e48
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-07-23 14:39:42 +0900

    ☔️ test: User의 포인트 관련 행위 테스트 작성
    > User의 행위 중 포인트에 대한 테스트 코드 작성
    > 포인트 충전
    > 포인트 사용

---
+++ src/test/kotlin/com/example/ecommerce/domain/user/model/request/UserTest.kt
@@ -0,0 +1,55 @@
+package com.example.ecommerce.domain.user.model.request
+
+import org.junit.jupiter.api.*
+
+class UserTest {
+
+    /*
+    * 포인트 테스트
+    * */
+    @Test
+    fun `0보다 큰 금액이면 포인트를 적립한다`() {
+        // given
+        val user = User(id = 1L, name = "User1", point = 0);
+
+        // when
+        user.chargePoint(100)
+
+        /*
+        * STUDY: assertEquals(a, b)
+        * a와 b가 같으면 테스트 통과
+        *
+        * STUDY: assertThrows(IllegalArgumentException::class.java) { ... }
+        * IllegalArgumentException 예외가 발생해야 테스트 통과
+        * */
+
+        // then
+        Assertions.assertEquals(100, user.getPoint(user.id))
+    }
+
+
+    @Test
+    fun `0보다 큰 금액이 아니면 포인트를 적립할 수 없다`() {
+        // given
+        val user = User(id = 1L, name = "User1", point = 0);
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            user.chargePoint(-100)
+        }
+    }
+
+    @Test
+    fun `포인트를 사용한다`() {
+        // given
+        val user = User(id = 1L, name = "User1", point = 100);
+
+        // when
+        user.usePoint(50)
+
+        // then
+        Assertions.assertEquals(50, user.getPoint(user.id))
+    }
+
+
+}

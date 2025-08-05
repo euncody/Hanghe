ac3d6ea860640f1b143900c884296378d7e9304c
김은지 <dotagekeji96@gmail.com>
2025-08-05 22:16:53 +0900

    🛠 refactor: 피드백 반영
    > OrderService의 createOrder 메서드에서 사용자 잔액 확인 및 차감 로직이 구현
    > 주문 생성 시 UserService와 협업하여 결제 가능 여부를 확인하고 실제 포인트를 차감
    > 주문한 상품의 재고를 차감

--- src/main/kotlin/com/example/ecommerce/order/infrastructure/persistence/OrderRepositoryImpl.kt
+++ src/main/kotlin/com/example/ecommerce/order/infrastructure/persistence/OrderRepositoryImpl.kt
@@ -3,12 +3,17 @@ package com.example.ecommerce.order.infrastructure.persistence
 import com.example.ecommerce.order.OrderMapper
 import com.example.ecommerce.order.domain.Order
 import com.example.ecommerce.order.domain.OrderRepository
+import com.example.ecommerce.product.domain.ProductService
+import com.example.ecommerce.user.domain.UserService
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Repository
 
 @Repository
 class OrderRepositoryImpl (
     private val jpaRepository: OrderJpaRepository,
-    private val orderMapper: OrderMapper
+    private val orderMapper: OrderMapper,
+    private val userService: UserService,
+    private val productService: ProductService
 ) : OrderRepository {
 
     override fun findByOrderId(orderId: String): Order? {
@@ -16,7 +21,34 @@ class OrderRepositoryImpl (
         return orderMapper.toDomain(entity)
     }
 
+    @Transactional
     override fun save(order: Order): Order {
+        // 유저 확인
+        val userId = order.user.userId
+        val user = userService.getUserByUserId(userId)
+
+        // 잔액 확인 및 차감
+        val totalAmount = order.totalAmount
+        if (user.point < totalAmount) {
+            throw IllegalStateException("잔액이 부족합니다.")
+        }
+        userService.useUserPoint(userId, totalAmount)
+
+        // 재고 차감
+        for (item in order.orderItems) {
+            val productKey = item.productKey
+                ?: throw IllegalArgumentException("상품 키가 없습니다.")
+
+            val product = productService.getProductKey(productKey)
+            if (product.amount < item.quantity) {
+                throw IllegalStateException("상품(${product.productName})의 재고가 부족합니다.")
+            }
+
+            val updatedProduct = product.decreaseStock(item.quantity)
+            productService.updateProduct(updatedProduct)
+        }
+
+        // 주문 저장
         val entity = orderMapper.toEntity(order)
         val savedEntity = jpaRepository.save(entity)
         return orderMapper.toDomain(savedEntity)
--- src/main/kotlin/com/example/ecommerce/product/domain/ProductRepository.kt
+++ src/main/kotlin/com/example/ecommerce/product/domain/ProductRepository.kt
@@ -1,8 +1,11 @@
 package com.example.ecommerce.product.domain
 
+import java.security.Key
+
 interface ProductRepository {
     fun save(product: Product): Product
     fun findByCode(code: String): Product?
+    fun findByKey(productKey: Long) : Product?
     fun findAll(): List<Product>
     fun update(product: Product): Product
     fun delete(productCode: String)
